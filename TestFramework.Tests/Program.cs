20107c353e8204d7bc39a76042f23f2f75e4ba47
Stefan Boos <kontakt@boos.systems>
2024-11-04 17:57:39 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:TestFramework.Tests/Program.cs

--- TestFramework.Tests/Program.cs
+++ TestFramework.Tests/Program.cs
@@ -9,10 +9,8 @@ using System.Diagnostics;
 Console.WriteLine(Directory.GetCurrentDirectory());
 
 var testrunner = new Process();
-testrunner.StartInfo.FileName = "TestFramework.exe";
-testrunner.StartInfo.WorkingDirectory = Path.Combine(
-    Directory.GetCurrentDirectory(), 
-    "TestFramework","bin","Debug","net8.0"
-);
+testrunner.StartInfo.FileName = "/Users/stefan/source/small-coding-dojo/smcd-csharp-testframework/TestFramework/bin/Debug/net8.0/TestFramework";
+testrunner.StartInfo.UseShellExecute = false;
+testrunner.StartInfo.WorkingDirectory = "/Users/stefan/source/small-coding-dojo/smcd-csharp-testframework/TestFramework/bin/Debug/net8.0";
 
 testrunner.Start();
